fd04e8838e815e2de541e831453c540624614079
yaziddouassi <rabahdouassi@yahoo.fr>
2025-02-05 21:16:14 +0100

    Your message here

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*$directory = base_path('routes'); // Specify the directory
-$fileName = 'saphir.php'; // Specify the file name
-
-$filePath = $directory . DIRECTORY_SEPARATOR . $fileName;
-
-if (file_exists($filePath)) {
-    require_once $filePath;
-} */
-
 
 Route::middleware('web')->group(function () {
    
